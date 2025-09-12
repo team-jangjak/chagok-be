ac87c43af353aa8a0d079e8d55aad228fb87caaf
Devy17 <devyks107@gmail.com>
2025-09-12 16:31:01 +0900

    First Commit

---
+++ src/main/java/com/jangjak/chagok/ChagokApplication.java
@@ -0,0 +1,13 @@
+package com.jangjak.chagok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChagokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChagokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jangjak/chagok/ChagokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jangjak.chagok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChagokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
