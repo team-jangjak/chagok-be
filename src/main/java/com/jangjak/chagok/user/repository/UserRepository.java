1a80608d8a7ac70265c9a5c521aa2c955813c107
hyeonji <guswl1424@gmail.com>
2025-09-17 16:36:00 +0900

    feat: user 관련 엔티티, 레파지토리 등 생성

---
+++ src/main/java/com/jangjak/chagok/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.jangjak.chagok.user.repository;
+
+import com.jangjak.chagok.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findByOauthId(Long oauthId);
+}
