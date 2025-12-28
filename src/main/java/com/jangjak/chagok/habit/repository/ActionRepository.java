f2674e08dd6091d9874b4e66a03132458a79f2a6
Devy17 <devyks107@gmail.com>
2025-12-28 16:19:27 +0900

    Param 어노테이션 추가

--- src/main/java/com/jangjak/chagok/habit/repository/ActionRepository.java
+++ src/main/java/com/jangjak/chagok/habit/repository/ActionRepository.java
@@ -12,28 +12,33 @@ import java.util.List;
 public interface ActionRepository extends JpaRepository<Action, Long> {
     @Query(
             value = """
-                SELECT *
-                FROM action
-                WHERE valid_end_at > :now 
-                AND valid_start_at <= :now 
-                AND habit_id = :habitId
-            """,
+                        SELECT *
+                        FROM action
+                        WHERE valid_end_at > :now 
+                        AND valid_start_at <= :now 
+                        AND habit_id = :habitId
+                    """,
             nativeQuery = true
     )
     List<Action> getActionsByHabitId(
             @Param("now") LocalDateTime now,
             @Param("habitId") Long habitId
     );
+
     boolean existsByCheckMethodId(Long checkMethodId);
 
     @Modifying
     @Query(
             value = """
-                UPDATE action
-                SET valid_end_at = :validStDt
-                WHERE habit_id = :habitId
-                AND valid_end_at = :max
-            """, nativeQuery = true
+                        UPDATE action
+                        SET valid_end_at = :validStDt
+                        WHERE habit_id = :habitId
+                        AND valid_end_at = :max
+                    """, nativeQuery = true
     )
-    void expireActions(Long habitId, LocalDateTime validStDt, LocalDateTime max);
+    void expireActions(
+            @Param("habitId") Long habitId,
+            @Param("validStDt") LocalDateTime validStDt,
+            @Param("max") LocalDateTime max
+    );
 }
